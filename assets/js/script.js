62b6bb634c3a33579415a491cac5ed0a647dbbdf
phillywhitty <philipwhitty92@yahoo.ie>
2023-03-31 12:08:42 +0000

    set up structure of quiz functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runQuiz() {
+
+}
+
+function checkAnwser() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incementWrongAnswer() {
+
+}
+
+function displayFirstQuestion() {
+
+}
+
+function displaySecondQuestion() {
+
+}
+
+function displayThirdQuestion() {
+
+}
+
+function displayFourthQuestion() {
+
+}
+
+function displayFifthQuestion() {
+
+}
