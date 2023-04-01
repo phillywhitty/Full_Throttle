186c28a2152f44d74accf43f37b1f0debb79560a
phillywhitty <philipwhitty92@yahoo.ie>
2023-04-01 20:44:13 +0000

    add question into game.js and set up basic js structure

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,82 @@
 const question = document.getElementById('question');
-const choices = document.getElementsByClassName("choice-text");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+
+
+    {
+        question: "The modern F1 Championship started in 1950. On which track was the first race held?",
+        choice1: "Bahrain Circuit",
+        choice2: "Shanghai Circuit",
+        choice3: "Silverstone",
+        choice4: "Nürburgring",
+        answer: 3
+
+    },
+
+    {
+        question: "Who is the only Dutch driver to have won a F1 Grand Prix?",
+        choice1: "Gijs van Lennep",
+        choice2: "Huub Rothengatter",
+        choice3: "Jan Lammers",
+        choice4: "Max Verstappen",
+        answer: 4
+    },
+
+    {
+        question: "World Rally Car engines have how many cylinders?",
+        choice1: "2",
+        choice2: "4",
+        choice3: "6",
+        choice4: "8",
+        answer: 4
+    },
+
+    {
+        question: "In rally racing, what does the co-driver do?",
+        choice1: "Sits there for the pure fun of it",
+        choice2: "Communicates back to the rally team",
+        choice3: "Assists the driver using navigation notes",
+        choice4: "Changes the radio station for the driver",
+        answer: 3
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+//CCONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+};
+
+startGame();
