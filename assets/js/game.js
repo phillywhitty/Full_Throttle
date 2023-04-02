bf14e3067bbe40d662773ca3a721a42238143196
phillywhitty <philipwhitty92@yahoo.ie>
2023-04-02 11:12:48 +0000

    add in newQuestion function

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,15 +68,29 @@ let questions = [
 
 ]
 
-//CCONSTANTS
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 4;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
 };
 
 startGame();
