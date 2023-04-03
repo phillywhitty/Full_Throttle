1ebd48747a72af405e9079dd5ddc3492752beeef
phillywhitty <philipwhitty92@yahoo.ie>
2023-04-03 12:45:12 +0000

    add save button disabled function, user must log input

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,8 @@
 const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
 
 username.addEventListener('keyup', () => {
-    console.log(username.value)
+    saveScoreBtn.disabled = !username.value;
 })
 
 saveHighScore = e => {
