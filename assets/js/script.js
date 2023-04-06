7c5d83a1be2d02653a643ae1aa039a4b3f7fb9bc
phillywhitty <philipwhitty92@yahoo.ie>
2023-04-06 08:11:49 +0000

    remove js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,13 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
-
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-
+//Quiz Questions
 let questions = [
-
-
     {
-        question: "The modern F1 Championship started in 1950. On which track was the first race held?",
+        question:
+            "The modern F1 Championship started in 1950. On which track was the first race held?",
         choice1: "Bahrain Circuit",
         choice2: "Shanghai Circuit",
         choice3: "Silverstone",
         choice4: "Nürburgring",
-        answer: 3
-
+        answer: 3,
     },
 
     {
@@ -28,7 +16,7 @@ let questions = [
         choice2: "Huub Rothengatter",
         choice3: "Jan Lammers",
         choice4: "Max Verstappen",
-        answer: 4
+        answer: 4,
     },
 
     {
@@ -37,7 +25,7 @@ let questions = [
         choice2: "4",
         choice3: "6",
         choice4: "8",
-        answer: 2
+        answer: 2,
     },
 
     {
@@ -46,94 +34,6 @@ let questions = [
         choice2: "Communicates back to the rally team",
         choice3: "Assists the driver using navigation notes",
         choice4: "Changes the radio station for the driver",
-        answer: 3
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
-
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
-    }
-
-    questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-
-
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-};
-
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-        if (classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
-
-    });
-});
-
-incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
-}
-
-
-startGame();
+        answer: 3,
+    },
+];
