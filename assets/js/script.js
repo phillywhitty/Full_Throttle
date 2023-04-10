6c1ca6f9b514207d2648734d68059ef8f7a36ea5
Philip Whitty <120309797+phillywhitty@users.noreply.github.com>
2023-04-10 13:57:42 +0000

    add in question counter element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
-/* A reference to the HTML element with id "question" */
+/* A reference to the HTML element with id "question". */
 const question = document.getElementById("question");
 
 /* The querySelectorAll method to find all HTML elements with class "choice-text"
-   and convert the resulting NodeList into an array using the spread operator */
+   and convert the resulting NodeList into an array using the spread operator. */
 const choices = [...document.querySelectorAll(".choice-text")];
 
-/* A variable to retrieve score ID
-Tracking the user's score
-Tracking the current question number
-An array to store the available questions
-An empty object for the current question */
+const questionCounterText = document.getElementById('questionCounter');
+
+/* A variable to retrieve score ID.
+   Tracking the user's score.
+   Tracking the current question number.
+   An array to store the available questions.
+   An empty object for the current question. */
 const scoreText = document.getElementById("score");
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 let currentQuestion = {};
 
-// A boolean to indicate whether answers are being accepted
+// A boolean to indicate whether answers are being accepted.
 let acceptingAnswers = false;
 
-// A constant for the bonus awarded for a correct answer & max questions for the quiz
+// A constant for the bonus awarded for a correct answer & max questions for the quiz.
 const EXACT_BONUS = 1;
 const QUESTIONS_LIMIT = 10;
 
@@ -122,10 +124,12 @@ let questions = [
 // FUNCTIONS //
 
 // FUNCTION TO START THE GAME //
-// Reset the question counter and the users score to 0
-// Create a new array of available questions by copying the original array
-// This is done using the slice method to create a new array with the same elements
-// Get a new question and display it to the user
+/* Reset the question counter and the users score to 0.
+   Create a new array of available questions by copying the original array.
+   This is done using the slice method to create a new array with the same elements.
+   Get a new question and display it to the user.
+ */
+
 startGame = () => {
   questionCounter = 0;
   score = 0;
@@ -134,8 +138,9 @@ startGame = () => {
 };
 
 // THIS FUNCTION GETS A NEW QUESTION //
-/* Check if there are any available questions left or if the maximum number of questions has been reached
-/* If there are no available questions or the maximum number of questions has been reached, redirect to the end page */
+/* Check if there are any available questions left or if the maximum number of questions has been reached.
+   If there are no available questions or the maximum number of questions has been reached, redirect to the end page. 
+*/
 
 function getNewQuestion() {
   if (availableQuesions.length === 0 || questionCounter >= QUESTIONS_LIMIT) {
@@ -143,19 +148,24 @@ function getNewQuestion() {
   }
 
   /* Increment the question counter 
-  Generate a random index for the question
-  Get the current question using the randomly generated index
-  Set the question text to be the current question's question property
+  // Set the text of the question counter element to display the current question number and total number of questions
+     Generate a random index for the question
+     Get the current question using the randomly generated index
+     Set the question text to be the current question's question property
   */
+
   questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + QUESTIONS_LIMIT;
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
   currentQuestion = availableQuesions[questionIndex];
   question.innerText = currentQuestion.question;
 
   // FUNCTION FOR SELECTING MY ANSWERS/CHOICES //
-  /* Repeats over each choice element
-     Get the choice's number attribute
-     Set the choice's text to be the corresponding choice from the current question */
+  /* Repeats over each choice element.
+     Get the choice's number attribute.
+     Set the choice's text to be the corresponding choice from the current question .
+  */
+
   choices.forEach((choice) => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
@@ -168,30 +178,32 @@ function getNewQuestion() {
   acceptingAnswers = true;
 }
 
-/* Iterate over each choice element 
-   A click event listener to the choice element
-   If the user isn't currently allowed to select an answer, return early and don't do anything */
+/* Repeat over each choice element 
+   A click event listener to the choice element.
+   If the user isn't currently allowed to select an answer, return early and don't do anything. 
+*/
 
   choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     if (!acceptingAnswers) return;
 
-    /* Set acceptingAnswers to false, so the user can't select another answer until I tell them
-     * Get the choice element that the user clicked on
-     * Get the answer number from the selected choice element's dataset
-     * Set a variable to hold the class to apply based on whether the answer is correct or incorrect */
+    /* Set acceptingAnswers to false, so the user can't select another answer until I tell them.
+       Get the choice element that the user clicked on.
+       Get the answer number from the selected choice element's dataset.
+       Set a variable to hold the class to apply based on whether the answer is correct or incorrect .
+    */
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
     //FUNCTION FOR STYLING BACKGROUNDS & ICREMENTING SCORE//
-
   /* Sets the class to "correct" and changes background to green
-  Otherwise, it sets the class to "incorrect" and change background to red
-  It then checks if the class to apply is "correct"
-  If so, it increments the score by 1 to the correct bonus amount
+     Otherwise, it sets the class to "incorrect" and change background to red
+     It then checks if the class to apply is "correct"
+     If so, it increments the score by 1 to the correct bonus amount
+  */ 
 
-*/ let classToApply;
+    let classToApply;
     if (selectedAnswer == currentQuestion.answer) {
       classToApply = "correct";
     } else {
@@ -202,12 +214,13 @@ function getNewQuestion() {
       incrementScore(EXACT_BONUS);
     }
 
-    // Add a class to the selected choice's parent element to show whether the answer was correct or incorrect
+    // Add a class to the selected choice's parent element to show whether the answer was correct or incorrect.
     selectedChoice.parentElement.classList.add(classToApply);
 
     // TIMEOUT FUNCTION //
-    /* Wait 1 second and then remove the class from the selected choice's parent element and get a new question
-       Wait for one second before removing the class and getting a new question */
+    /* Wait 1 second and then remove the class from the selected choice's parent element and get a new question.
+       Wait for one second before removing the class and getting a new question.
+     */
 
     setTimeout(function () {
       selectedChoice.parentElement.classList.remove(classToApply);
@@ -215,15 +228,17 @@ function getNewQuestion() {
     }, 1000);
   });
 });
+
 // INCREMENT SCORE FUNCTION //
 /* A function that increments the score by 1.
    It then gives the amount(1) to the score variable and
-   updates the text of the score element to display the new score */
+   updates the text of the score element to display the new score.
+ */
 
 incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
 
-// This code will be executed once all other functions are defined
+// This code will be executed once all other functions are defined.
 startGame();
